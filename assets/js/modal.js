3891a5125f2543cd0b5c2e999aa1ef2e4e3052a0
Cesar Julian Salazar <trabajos.cesar23@gmail.com>
2021-08-25 15:17:59 -0500

    Ojala funcione

---
+++ assets/js/modal.js
@@ -0,0 +1,56 @@
+let modal = document.getElementById('miModal');
+let flex = document.getElementById('flex');
+let abrir = document.getElementById('abrir');
+let cerrar = document.getElementById('close');
+
+
+let modal2 = document.getElementById('miModal2');
+let cerrar2 = document.getElementById('close2');
+
+let modal3 = document.getElementById('miModal3');
+let cerrar3 = document.getElementById('close3');
+
+abrir.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+abrir2.addEventListener('click', function(){
+    modal2.style.display = 'block';
+});
+
+abrir3.addEventListener('click', function(){
+    modal3.style.display = 'block';
+});
+
+
+cerrar.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
+cerrar2.addEventListener('click', function(){
+    modal2.style.display = 'none';
+});
+cerrar3.addEventListener('click', function(){
+    modal3.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    
+    if(e.target == flex){
+        modal.style.display = 'none';
+    }
+});
+
+window.addEventListener('click', function(e){
+    
+    if(e.target == flex2){
+        modal2.style.display = 'none';
+    }
+})
+
+window.addEventListener('click', function(e){
+    
+    if(e.target == flex3){
+        modal3.style.display = 'none';
+    }
+})
+
